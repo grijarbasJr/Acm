dffc1e18620545430a03245d034e7f1c04506d0d
Grijarbas S. S. Junior <sirgrijarbas1@gmail.com>
2021-03-29 22:20:05 -0300

    att29/03

--- modelos/models.py
+++ modelos/models.py
@@ -16,8 +16,8 @@ class Operator(models.Model):
 
 class Machine(models.Model):
     status_choices = (
-        ("V","Vendido"),
-        ("P","Pertence"),
+        ("V", "Vendido"),
+        ("P", "Pertence"),
     )
     machine_secure_init = models.DateField(max_length=10, null=False)
     machine_secure_end = models.DateField(max_length=10, null=False)
@@ -30,8 +30,8 @@ class Machine(models.Model):
 
 class Revision(models.Model):
     status_choices = (
-        ("P","Parcial"),
-        ("G","Geral"),
+        ("P", "Parcial"),
+        ("G", "Geral"),
     )
     machine_revision = models.CharField(max_length=10, choices=status_choices, blank=False, null=False)
     machine_revision_date = models.DateField(max_length=10, null=False)
@@ -47,6 +47,38 @@ class Service(models.Model):
     money_advance = models.DecimalField(decimal_places=2, max_digits=10)
     hour_valor = models.DecimalField(decimal_places=2, max_digits=4)
     total_value_service = models.DecimalField(decimal_places=2, max_digits=10)
+
+
+class Smachines(models.Model):
+    status_choices = (
+        ("P", "Pago"),
+        ("G", "Não pago"),
+    )
+    sec_name_machine = models.ForeignKey(Machine, on_delete=models.RESTRICT)
+    secure_final_date = models.DateField(max_length=10, null=False)
+    secure_initial_date = models.DateField(max_length=10, null=False)
+    secure_text_d = models.CharField(max_length=50, blank=False, null=False)
+    s_parcel_numb = models.IntegerField(null=False)
+    s_parcel_total = models.DecimalField(decimal_places=2, max_digits=4)
+    s_parc_pag = models.CharField(max_length=10, choices=status_choices, blank=False, null=False)
+
+
+class Parcmachine(models.Model):
+    status_choices = (
+        ("P", "Pago"),
+        ("G", "Não pago"),
+    )
+    parc_name_machine = models.ForeignKey(Machine, on_delete=models.RESTRICT)
+    parc_machine_value = models.DecimalField(decimal_places=2, max_digits=10)
+    parc_parcel = models.IntegerField(null=False)
+    parc_pag = models.CharField(max_length=10, choices=status_choices, blank=False, null=False)
+
+
+
+
+
+
+
 
 
 
